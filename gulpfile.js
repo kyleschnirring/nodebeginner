a2a6eb95c7293136dd2da63a11f9a589260bda21
kyleschnirring <kyleschnirring@gmail.com>
2016-05-09 17:23:05 -0700

    i likey

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,65 @@
 'use strict';
 
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-var mocha = require('gulp-mocha');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
 
-var paths = ['*.js', 'lib/*.js', 'test/*.js'];
+const paths = ['*.js', 'bitmap-mosnter/lib/*.js', 'bitmap-mosnter/test/*.js'];
 
 gulp.task('eslint', function() {
   return gulp.src(paths)
-    .pipe(eslint())
+    .pipe(eslint(
+      {
+        'rules': {
+          'no-console': 0,
+          'indent': [
+            2,
+            2
+          ],
+          'quotes': [
+            2,
+            'single'
+          ],
+          'linebreak-style': [
+            2,
+            'unix'
+          ],
+          'semi': [
+            2,
+            'always'
+          ]
+        },
+        'env': {
+          'es6': true,
+          'node': true,
+          'browser': true
+        },
+        'globals': {
+          'describe': false,
+          'it': false,
+          'beforeEach': false,
+          'afterEach': false,
+          'before': false,
+          'after': false
+        },
+        'ecmaFeatures': {
+          'modules': true,
+          'experimentalObjectRestSpread': true,
+          'impliedStrict': true
+        },
+        'extends': 'eslint:recommended'
+      }
+    ))
     .pipe(eslint.format());
 });
 
 gulp.task('test', function() {
   return gulp.src(paths)
-    .pipe(mocha());
+  .pipe(mocha());
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['lib/*', 'test/*'], ['test', 'eslint']);
+  gulp.watch(['bitmap-mosnter/lib/**', 'bitmap-mosnter/test/**'], ['test', 'eslint']);
 });
 
-gulp.task('default', ['eslint', 'test']);
-
+gulp.task('default', ['eslint', 'test', 'watch']);
